c6ce8bd3677dff6e0333300fa8c4f1913c770015
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-10-10 23:17:48 +0200

    [FIX] indentation

--- js/main.js
+++ js/main.js
@@ -1,58 +1,57 @@
 window.onload = () => {
-  'use strict';
+    'use strict';
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js');
 
-    var url = new URL(window.location.toString()),
-        youtubeUrl = new URL(decodeURI(url.searchParams.get('text')) || 'https://youtu.be/CNEpwvfg_Zo'),
-        vid = youtubeUrl.pathname.substr(1),
-        audio_streams = {},
-        audio_tag = document.getElementById('youtube');
+        var url = new URL(window.location.toString()),
+            youtubeUrl = new URL(decodeURI(url.searchParams.get('url'))),
+            vid = youtubeUrl.pathname.substr(1),
+            audio_streams = {},
+            audio_tag = document.getElementById('youtube');
 
-    fetch("https://"+vid+"-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https%3A%2F%2Fwww.youtube.com%2Fget_video_info%3Fvideo_id%3D" + vid).then(response => {
-      if (response.ok) {
-        response.text().then(data => {
+        fetch("https://" + vid + "-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https%3A%2F%2Fwww.youtube.com%2Fget_video_info%3Fvideo_id%3D" + vid).then(response => {
+            if (response.ok) {
+                response.text().then(data => {
 
-          var data = parse_str(data),
-              streams = (data.url_encoded_fmt_stream_map + ',' + data.adaptive_fmts).split(',');
+                    var data = parse_str(data),
+                        streams = (data.url_encoded_fmt_stream_map + ',' + data.adaptive_fmts).split(',');
 
-          streams.forEach(function(s, n) {
-            var stream = parse_str(s),
-                itag = stream.itag * 1,
-                quality = false;
-            console.log(stream);
-            switch (itag) {
-              case 139:
-                quality = "48kbps";
-                break;
-              case 140:
-                quality = "128kbps";
-                break;
-              case 141:
-                quality = "256kbps";
-                break;
+                    streams.forEach(function (s, n) {
+                        var stream = parse_str(s),
+                            itag = stream.itag * 1,
+                            quality = false;
+                        console.log(stream);
+                        switch (itag) {
+                            case 139:
+                                quality = "48kbps";
+                                break;
+                            case 140:
+                                quality = "128kbps";
+                                break;
+                            case 141:
+                                quality = "256kbps";
+                                break;
+                        }
+                        if (quality) audio_streams[quality] = stream.url;
+                    });
+
+                    console.log(audio_streams);
+
+                    audio_tag.src = audio_streams['128kbps'];
+                    audio_tag.play();
+                })
             }
-            if (quality) audio_streams[quality] = stream.url;
-          });
-
-          console.log(audio_streams);
-
-          audio_tag.src = audio_streams['128kbps'];
-          audio_tag.play();
-        })
-      }
-    });
-
-    function parse_str(str) {
-      return str.split('&').reduce(function(params, param) {
-        var paramSplit = param.split('=').map(function(value) {
-          return decodeURIComponent(value.replace('+', ' '));
         });
-        params[paramSplit[0]] = paramSplit[1];
-        return params;
-      }, {});
+
+        function parse_str(str) {
+            return str.split('&').reduce(function (params, param) {
+                var paramSplit = param.split('=').map(function (value) {
+                    return decodeURIComponent(value.replace('+', ' '));
+                });
+                params[paramSplit[0]] = paramSplit[1];
+                return params;
+            }, {});
+        }
     }
-  }
 };
--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
-  '/youtubelistener/',
-  '/youtubelistener/index.html',
-  '/youtubelistener/css/style.css',
-  '/youtubelistener/js/main.js'
-];
+const cacheName = 'hello-pwa',
+    filesToCache = [
+        '/youtubelistener/',
+        '/youtubelistener/index.html',
+        '/youtubelistener/css/style.css',
+        '/youtubelistener/js/main.js'
+    ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
 });
