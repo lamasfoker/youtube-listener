b21a79ad1f2f62b100927e7cb078e31f580cfa4d
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-10-11 10:02:36 +0200

    [REFACTOR]

--- js/main.js
+++ js/main.js
@@ -1,52 +1,60 @@
-window.onload = () => {
+window.onload = async () => {
     'use strict';
 
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js');
+        await navigator.serviceWorker.register('./sw.js');
+        const audioTag = document.getElementById('youtube');
+        let url = new URL(window.location.href.toString()),
+            youtubeUrl,
+            videoId,
+            audioStreams = {};
 
-        var url = new URL(window.location.toString()),
-            youtubeUrl = new URL(decodeURI(url.searchParams.get('url'))),
-            vid = youtubeUrl.pathname.substr(1),
-            audio_streams = {},
-            audio_tag = document.getElementById('youtube');
+        if (!url.searchParams.get('url')) {
+            return;
+        }
+        youtubeUrl = new URL(url.searchParams.get('url'));
+        videoId = youtubeUrl.pathname.substr(1);
 
-        fetch("https://" + vid + "-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https%3A%2F%2Fwww.youtube.com%2Fget_video_info%3Fvideo_id%3D" + vid).then(response => {
-            if (response.ok) {
-                response.text().then(data => {
+        let apiUrl = "https://" + videoId + "-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https%3A%2F%2Fwww.youtube.com%2Fget_video_info%3Fvideo_id%3D" + videoId;
 
-                    var data = parse_str(data),
-                        streams = (data.url_encoded_fmt_stream_map + ',' + data.adaptive_fmts).split(',');
-
-                    streams.forEach(function (s, n) {
-                        var stream = parse_str(s),
-                            itag = stream.itag * 1,
-                            quality = false;
-                        console.log(stream);
-                        switch (itag) {
-                            case 139:
-                                quality = "48kbps";
-                                break;
-                            case 140:
-                                quality = "128kbps";
-                                break;
-                            case 141:
-                                quality = "256kbps";
-                                break;
-                        }
-                        if (quality) audio_streams[quality] = stream.url;
-                    });
-
-                    console.log(audio_streams);
-
-                    audio_tag.src = audio_streams['128kbps'];
-                    audio_tag.play();
-                })
-            }
+        let response = await fetch(apiUrl, {
+            method: 'GET'
         });
+
+        if (response.ok) {
+            response.text().then(data => {
+
+                data = parse_str(data);
+                let streams = (data.url_encoded_fmt_stream_map + ',' + data.adaptive_fmts).split(',');
+
+                streams.forEach(function (s, n) {
+                    let stream = parse_str(s),
+                        itag = stream.itag * 1,
+                        quality = false;
+                    switch (itag) {
+                        case 139:
+                            quality = "48kbps";
+                            break;
+                        case 140:
+                            quality = "128kbps";
+                            break;
+                        case 141:
+                            quality = "256kbps";
+                            break;
+                    }
+                    if (quality) {
+                        audioStreams[quality] = stream.url;
+                    }
+                });
+
+                audioTag.src = audioStreams['128kbps'];
+                audioTag.play();
+            })
+        }
 
         function parse_str(str) {
             return str.split('&').reduce(function (params, param) {
-                var paramSplit = param.split('=').map(function (value) {
+                let paramSplit = param.split('=').map(function (value) {
                     return decodeURIComponent(value.replace('+', ' '));
                 });
                 params[paramSplit[0]] = paramSplit[1];
