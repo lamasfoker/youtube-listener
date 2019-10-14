6bf79d1959d2c21679195241d0367b61e15ed603
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-10-14 22:09:15 +0200

    Remove comments

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -14,7 +14,6 @@ const cacheName = "youtubelistener",
         "/youtubelistener/assets/sounds/failed.mp4"
     ];
 
-/* Start the service worker and cache all of the app"s content */
 self.addEventListener("install", function (e) {
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
@@ -23,7 +22,6 @@ self.addEventListener("install", function (e) {
     );
 });
 
-/* Serve cached content when offline */
 self.addEventListener("fetch", function (e) {
     e.respondWith(
         caches.match(e.request).then(function (response) {
