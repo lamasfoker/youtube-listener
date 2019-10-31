e57970ecf0c109317785cab2fe4b34e843eb13e2
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-10-31 16:32:56 +0100

    [REFACTOR] Remove AmplitudeJS and use HowlerJS instead

--- app.js
+++ app.js
@@ -4,25 +4,39 @@ import CompatibilityChecker from "./service/CompatibilityChecker.js";
 import YouTubeRequest from "./service/YouTubeRequest.js";
 import AudioExtractor from "./service/AudioExtractor.js";
 import StreamQuality from "./service/StreamQuality.js";
+import TimeFormatter from "./service/TimeFormatter.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const screen = document.querySelector("html");
-    screen.style.visibility = "hidden";
     try {
         CompatibilityChecker.checkServiceWorker();
     } catch (e) {
         return alert(e);
     }
     await navigator.serviceWorker.register("./ServiceWorker.js");
-    const progressBar = document.querySelector("#song-played-progress");
+    const progressBar = document.querySelector(".song-played-progress");
     const qualityToggle = document.querySelector("#toggle");
+    const playerButton = document.querySelector(".play-pause");
+    const currentTime = document.querySelector(".current-time");
+    const changePlayerButtonStatus = () => {
+        if (playerButton.classList.contains('paused')) {
+            playerButton.classList.add("playing");
+            playerButton.classList.remove("paused");
+        } else {
+            playerButton.classList.add("paused");
+            playerButton.classList.remove("playing");
+        }
+    };
+    const updateProgressBar = () => {
+        let seek = sound.seek();
+        currentTime.innerHTML = TimeFormatter.format(seek);
+        progressBar.value = seek / sound.duration();
+    };
     let audio;
 
     if (StreamQuality.getStreamQuality() === StreamQuality.high) {
         qualityToggle.checked = true;
     }
     qualityToggle.onclick = StreamQuality.changeStreamQuality;
-
     try {
         CompatibilityChecker.checkStandaloneMode();
         let body = await YouTubeRequest.getBody();
@@ -38,26 +52,41 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
     }
 
-    Amplitude.init({
-        "bindings": {
-            37: "prev",
-            39: "next",
-            32: "play_pause"
+    document.querySelector(".song-name").innerHTML = audio.title;
+    document.querySelector(".song-author").innerHTML = audio.author;
+    document.querySelector(".thumbnail").src = audio.thumbnailUrl;
+
+    const sound = new Howl({
+        src: StreamQuality.getStreamUrl(audio.streams),
+        html5: true,
+        preload: true,
+        onload: () => {
+            document.querySelector(".duration").innerHTML = TimeFormatter.format(sound.duration());
+            setInterval(updateProgressBar, 100);
         },
-        "songs": [
-            {
-                "name": audio.title,
-                "artist": audio.author,
-                "url": StreamQuality.getStreamUrl(audio.streams),
-                "cover_art_url": audio.thumbnailUrl
-            }
-        ]
+        onplay: () => {
+            changePlayerButtonStatus();
+        },
+        onend: () => {
+            changePlayerButtonStatus();
+        },
+        onpause: () => {
+            changePlayerButtonStatus();
+        }
     });
 
-    progressBar.addEventListener("click", function (e) {
-        let offset = this.getBoundingClientRect(),
-            cursor = e.x - offset.left;
-        Amplitude.setSongPlayedPercentage((parseFloat(cursor) / parseFloat(this.offsetWidth)) * 100);
-    });
-    screen.style.visibility = "visible";
+    playerButton.onclick = () => {
+        if (sound.playing()) {
+            sound.pause();
+        } else {
+            sound.play();
+        }
+    };
+
+    progressBar.onclick = (e) => {
+        let offset = progressBar.getBoundingClientRect(),
+            cursor = e.x - offset.left,
+            seek = parseFloat(cursor) / parseFloat(progressBar.offsetWidth);
+        sound.seek(seek * sound.duration());
+    };
 });
--- service/TimeFormatter.js
+++ service/TimeFormatter.js
@@ -6,11 +6,11 @@ const TimeFormatter = {
         let hours = Math.floor(duration / 3600);
         let minutes = Math.floor((duration - hours * 3600) / 60);
         let seconds = duration - hours * 3600 - minutes * 60;
-        let formated = String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0");
+        let formatted = String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0");
         if (hours > 0) {
-            formated = String(hours).padStart(2, "0") + ":" + duration;
+            formatted = String(hours).padStart(2, "0") + ":" + duration;
         }
-        return formated;
+        return formatted;
     }
 };
 
